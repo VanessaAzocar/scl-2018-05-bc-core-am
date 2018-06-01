47f126476d1783fa6600fd4298f839cee7817449
Vanessa Azócar <v.azocar.adasme@gmail.com>
2018-05-31 21:15:07 -0300

    ultimos arreglos del dom y cifrado

--- src/cipher.js
+++ src/cipher.js
@@ -1,46 +1,46 @@
 window.cipher = { /* Acá va tu código */
-  //CODIFICAR
-  encode: (decodedSend, decodedNumber) => {
+  //DECODIFICAR
+  decode: (decodedSend, decodedNumber) => {
     let result = "";
     let i =0;
       //iteracion que hara a mi frase
       for ( i ; i < decodedSend.length; i++) {
         //convierte mayuscula la frase incresada
         let tMayus= decodedSend.toUpperCase();
-        //Formula del cifrado 
-        fraseDecoded= (tMayus.charCodeAt(i) - 65 + parseInt(decodedNumber)) % 26 + 65;
         //para que aparezca el espacio que es en ASCII n°32
         if (tMayus.charCodeAt(i)===32){
           result += " ";
-          }else{ 
+        }else{ 
+          //Formula para decodificar 
+          let fraseDecoded= (tMayus.charCodeAt(i) - 65 - parseInt(decodedNumber)) % 26 + 65;
           //guarda cada resultado de cada letra para despues mostrar la frase completa
+          if (fraseDecoded < 65){
+          fraseDecoded += 26;
+          }
           result += String.fromCharCode(fraseDecoded);
         }   
       } 
     return result;
   },
   
-  //DECODIFICAR
-  decode: (codedSend, codedNumber) => {
+  //CODIFICAR
+  encode: (codedSend, codedNumber) => {
     let result = "";
     let i = 0;    
       //iteracion que hara a mi frase
       for ( i ; i < codedSend.length; i++) {
         //convierte mayuscula la frase incresada
         let tMayus= codedSend.toUpperCase(i);
+        //Formula para codificar 
+        let fraseCoded= (tMayus.charCodeAt(i) - 65 + parseInt(codedNumber)) % 26 + 65;
         //para que aparezca el espacio que es en ASCII n°32
         if (tMayus.charCodeAt(i) === 32){
           result += " ";           
-          }else {
-            //Formula del decifrado 
-            let fraseCoded= (tMayus.charCodeAt(i) - 65 - parseInt(codedNumber)) % 26 + 65;
-            //guarda cada resultado de cada letra para despues mostrar la frase completa
-            if (fraseCoded < 65) {
-            fraseCoded += 26;
-            }
+        }else {
+          //guarda cada resultado de cada letra para despues mostrar la frase completa
           result += String.fromCharCode(fraseCoded);
-          } 
+        } 
       }
     return result;
-  }
+  },
 }
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ window.onload = () => {
   const decoded = document.getElementById("btnEnterDecoded"); /*para traer el valor del boton*/    decoded.addEventListener ("click",() => { /*evento click*/
     let decodedSend = document.getElementById("sendDecoded").value; /*para guardar el texto que ingreso el usuario en texarea */
     let decodedNumber = document.getElementById("numberDecoded").value;/*para guardar el numero que ingreso el usuario en texarea */
-    let resultDecoded = window.cipher.encode(decodedSend, decodedNumber); /*para enlazar el texto decodificado*/
+    let resultDecoded = window.cipher.decode(decodedSend, decodedNumber); /*para enlazar el texto decodificado*/
     document.getElementById("msjDecoded").innerHTML = "El mensaje oculto decodificado es: "  + resultDecoded;/*que regrese el resultado*/
   })
 
@@ -14,8 +14,8 @@ window.onload = () => {
     coded.addEventListener ("click",() => {  /*evento click*/
     let codedSend = document.getElementById("receivedCoded").value;/*para guardar el texto que ingreso el usuario en texarea */
     let codedNumber = document.getElementById("numberCoded").value;/*para guardar el numero que ingreso el usuario en texarea */
-    let resultEncoded = window.cipher.decode(codedSend, codedNumber); /*para enlazar el texto codificado*/
-    document.getElementById("msjCoded").innerHTML = "<strong>El mensaje codificado es: </strong>" + "<strong>"+ resultEncoded + "</strong>" + "<br><br>*Ahora puedes copiar y enviar este mensaje, solo indicando el número de desplazamiento y será secreto!!!</br></br>"; /*que reguese el resultado*/
+    let resultCoded = window.cipher.encode(codedSend, codedNumber); /*para enlazar el texto codificado*/
+    document.getElementById("msjCoded").innerHTML = "<strong>El mensaje codificado es: </strong>" + "<strong>"+ resultCoded + "</strong>" + "<br><br>*Ahora puedes copiar y enviar este mensaje, solo indicando el número de desplazamiento y será secreto!!!</br></br>"; /*que reguese el resultado*/
   })
 
     
