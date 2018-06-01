947d72976710f59c252f9b5d2e59086f31a772b0
Vanessa Azócar <v.azocar.adasme@gmail.com>
2018-06-01 10:35:38 -0300

    cifrando considerando espcios

--- src/cipher.js
+++ src/cipher.js
@@ -5,15 +5,19 @@ window.cipher = { /* Acá va tu código */
     let i =0;
       //iteracion que hara a mi frase
       for ( i ; i < decodedSend.length; i++) {
-      //convierte mayuscula
+      //convierte mayuscula la frase incresada
       let tMayus= decodedSend.toUpperCase();
-      //Formula del cifrado
+      //Formula del cifrado 
       fraseCoded= (tMayus.charCodeAt(i) - 65 + parseInt(decodedNumber)) % 26 + 65;
-      //guarda cada resultado de cada letra para despues mostrar la frase completa 
-      result += String.fromCharCode(fraseCoded);
-  } 
-  return result;
-   
+      //para que aparezca el espacio que es en ASCII n°32
+      if (tMayus.charCodeAt(i)===32){
+        result += " ";
+        } else{ 
+        //guarda cada resultado de cada letra para despues mostrar la frase completa
+        result += String.fromCharCode(fraseCoded);
+        }   
+      } 
+    return result;
   },
 
 
