02be6d0d7d0abc980a37d5fb1b8f31ff0827a23a
Vanessa Azócar <v.azocar.adasme@gmail.com>
2018-06-01 00:51:19 -0300

    construyendo DOM

--- src/cipher.js
+++ src/cipher.js
@@ -1,7 +1,22 @@
 window.cipher = {
   encode: () => {
     /* Acá va tu código */
+    //Funciones trozos de codigos reutilizable
+    const frase = "vanessa azocar";
+    let result = "";
+    let i =0;
+      for ( i ; i < frase.length; i++) {
+        fraseCoded= ((frase.charCodeAt(i)-65+numberDecoded))%26+65;
+           
+      result += frase[i].toUpperCase();
+      console.log(result)
+  }
+ 
+  iteracion();
+   
   },
+
+
   decode: () => {
     /* Acá va tu código */
   }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,20 @@
 /* Acá va tu código , (manejo del DOM)*/ 
-window.onload = => (){
-/*siempre espera que el usuario haga algo para que cuando el usuario haga click que es su funcion*/
- /*se hace la variable para guardar el texto y que busqye la clase que tiene text_hide y se busca la posicion que es 0 (la que esta en 1 lugar en el HTML en orden de escritura) */
- /*hide se pone en CSS diciendo que se oculte*/
+window.onload = => () {
 
  /* Boton DECODIFICAR */
-  const encoded = document.getElementById("btnEnterEncoded").addEventListener ("click",() => {
-    const codedSend = document.getElementById("sendDecoded").value;
-    const codednumber = document.getElementById("numberCoded").value;
-    document.getElementById("msjDecoded").innerHTML = "El mensaje oculto decodificado es:" ;
+  const decoded = document.getElementById("btnEnterDecoded"); /*para traer el valor del boton*/    decoded .addEventListener ("click",() => { /*evento click*/
+    let decodedSend = document.getElementById("sendDecoded")[0].value; /*para guardar el texto que ingreso el usuario en texarea */
+    let decodednumber = document.getElementById("numberDecoded").value;/*para guardar el numero que ingreso el usuario en texarea */
+    document.getElementById("msjDecoded").innerHTML = "El mensaje oculto decodificado es:" ;/*que regrese el resultado*/
   })
+
   /* Boton CODIFICAR */
-  const coded= document.getElementById("btnEnterCoded");
-    encoded.addEventListener ("click",() => {
-    let codedSend = document.getElementById("receivedCoded")[0].value;
-    let codednumber = document.ElementsByClassName("numberCoded").value;
-    document.getElementById("msjCoded").innerHTML = "El mensaje codificado es:" + ""  + "*Ahora puedes copiar y enviar este mensaje, solo indicando el número de desplazamiento y será secreto!!!";
+  const coded= document.getElementById("btnEnterCoded");/*para traer el valor del boton*/ 
+    coded.addEventListener ("click",() => {  /*evento click*/
+    let codedSend = document.getElementById("receivedCoded")[0].value ;/*para guardar el texto que ingreso el usuario en texarea */
+    let codednumber = document.ElementsByClassName("numberCoded").value;/*para guardar el numero que ingreso el usuario en texarea */
+    document.getElementById("msjCoded").innerHTML = "El mensaje codificado es:" + ""  + "*Ahora puedes copiar y enviar este mensaje, solo indicando el número de desplazamiento y será secreto!!!";/*que reguese el resultado*/
   })
 
     
-}
+  }
